5f1e3b51f0f1242fcc136d33c4f1d0f5a28bb861
Brandon Erkamaa <brandonerkamaa@gmail.com>
2022-05-23 16:47:25 -0500

    Created workflows

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
-const functionApi ='http://localhost:7071/api/GetResumeCounter';
+const functionApi ='https://getresumecountererkamaa.azurewebsites.net/api/GetResumeCounter?code=ZJXF3hAEUo9wpA3_4weNuMuCjUFV6OfarQFKfgIohirKAzFulsyhfQ==';
 
 const getVisitCount = () => {
     let count =30;
